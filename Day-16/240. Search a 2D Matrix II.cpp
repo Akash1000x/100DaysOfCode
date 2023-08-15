353e1e16e057c486f2577cb905cc4bf6701354ee
Akash Kumar Kumawat <kumawatap63@gmail.com>
2023-08-15 23:32:09 +0530

    first commit

--- Day-16/240. Search a 2D Matrix II.cpp
+++ Day-16/240. Search a 2D Matrix II.cpp
@@ -11,28 +11,44 @@ public:
         int col = matrix[0].size();
         
 
-        for(int i= 0;i<row;i++){
+        // for(int i= 0;i<row;i++){
             
-            int start = 0;
-            int end = col-1;
-            while(start<=end){
+        //     int start = 0;
+        //     int end = col-1;
+        //     while(start<=end){
 
-                int mid = start + (end-start)/2;
-                int element = matrix[i][mid];
-                if(element == target){
-                    return true;
-                }
+        //         int mid = start + (end-start)/2;
+        //         int element = matrix[i][mid];
+        //         if(element == target){
+        //             return true;
+        //         }
 
-                else if(element > target){
-                    end = mid -1;
-                }
+        //         else if(element > target){
+        //             end = mid -1;
+        //         }
 
-                else{
-                    start = mid +1;
-                }
+        //         else{
+        //             start = mid +1;
+        //         }
 
+        //     }
+            
+        // }
+        // return false;
+
+        int rowIndex = 0;
+        int colIndex = col-1;
+        while( rowIndex < row && colIndex >=0 ){
+            
+            if(matrix[rowIndex][colIndex] == target){
+                return true;
             }
-            
+            else if(matrix[rowIndex][colIndex] < target){
+                rowIndex++;
+            }
+            else{
+                colIndex--;
+            }
         }
         return false;
     }
