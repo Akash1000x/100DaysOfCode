b49a1cd99415ad9b9976ddc01f8e03d82f41ac23
Akash Kumar Kumawat <kumawatap63@gmail.com>
2023-08-24 01:38:38 +0530

    first commit

--- Day-19/917. Reverse Only Letters.cpp
+++ Day-19/917. Reverse Only Letters.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
+        int start = 0;
+        int end = s.size()-1;
+
+        while(start<=end){
+            if(isalpha(s[start]) && isalpha(s[end])){
+                swap(s[start] ,s[end]);
+                start++;
+                end--;
+            }
+            else if(!isalpha(s[start])){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+        return s;
+    }
+};
+
+
+
+//or
+
+
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
         int i=0;
         int j=s.length()-1;
         while(i<=j){
