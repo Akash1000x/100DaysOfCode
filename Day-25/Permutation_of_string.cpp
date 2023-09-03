6bb9a82a2748d63fb46216fd4daf5dce9d42b685
Akash Kumar Kumawat <kumawatap63@gmail.com>
2023-09-03 14:26:36 +0530

    first commit

---
+++ Day-25/Permutation_of_string.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void PermutationOfString(string &s,int n,int i){
+    if(i>=n){
+        cout<<s<<endl;
+        return;
+    }
+
+    //swaping
+    for(int j = i;j<n;j++){
+        //swap
+        swap(s[i],s[j]);
+
+        //recurision call
+        PermutationOfString(s,n,i+1);
+
+        //backtracking
+        swap(s[i],s[j]);
+    }
+}
+
+int main(){
+    string s  = "abc";
+    int n = s.size();
+    PermutationOfString(s,n,0);
+    return 0;
+}
