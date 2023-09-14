b8ad110fea2b60c3c4638b778e37956ad7406dba
Akash Kumar Kumawat <kumawatap63@gmail.com>
2023-09-14 23:10:18 +0530

    first commit

--- Day-33/287. Find the Duplicate Number.cpp
+++ Day-33/287. Find the Duplicate Number.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       
-        sort(nums.begin(),nums.end());
-            for(int j= 0;j<nums.size()-1;j++){
-                if(nums[j]==nums[j+1]){
-                    return nums[j];
-                }
-            }
+        class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        while(nums[0] != nums[nums[0]]){
+            
+            swap(nums[0],nums[nums[0]]);
+        }
+        return nums[0];
         
-        return 0;
+        // for(int i = 0;i<nums.size();i++){
+        //     int index = abs(nums[i]);
+        //     if(nums[index] <0  ){
+        //         return index;
+
+        //     }
+        //     else{
+        //         nums[index] *= (-1);
+        //     }
+
+        // }
+        // return -1;
+        
+       //or
+
+
+
+        // sort(nums.begin(),nums.end());
+        //     for(int j= 0;j<nums.size()-1;j++){
+        //         if(nums[j]==nums[j+1]){
+        //             return nums[j];
+        //         }
+        //     }
+        
+        // return 0;
     }
 };
