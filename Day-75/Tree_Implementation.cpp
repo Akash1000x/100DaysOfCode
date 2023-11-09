03e4bd37ef6ab6380480a42dd70ea340f06f8637
Akash Kumar Kumawat <kumawatap63@gmail.com>
2023-11-09 19:18:39 +0530

    first commit

---
+++ Day-75/Tree_Implementation.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// creating a node
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *builder()
+{
+
+    int data;
+    cout << "enter the element to insert:";
+    cin >> data;
+
+    // -1 data indicates that we have the leaf node (Base Case)
+    if (data == -1)
+    {
+        return NULL;
+    }
+    // Create the root node and hence solved 1 case
+    Node *root = new Node(data);
+
+    // Recursion will handle
+    cout << "Enter the data for left part of " << data << " Node" << endl;
+    root->left = builder();
+
+    cout << "Enter the data for right part of " << data << " Node" << endl;
+    root->right = builder();
+
+    return root;
+}
+
+
+
+int main()
+{
+    Node *root = builder();
+
+    return 0;
+}
